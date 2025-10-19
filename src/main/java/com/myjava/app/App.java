d3b31517be4845fd1b027befe58ed2aea3d010d0
edwardp20 <zhangxixi201268@outlook.com>
2025-10-19 16:06:32 +0800

    feat:Copper.class

--- src/main/java/com/myjava/app/App.java
+++ src/main/java/com/myjava/app/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "你好，欢迎来到本程序，版本请输入java -cp myjava-20251016-SNAPSHOT.jar com.myjava.app.Version" );
+        System.out.println("本程序目前有Copper,Version,ArithmeticcoperatorDemo,App(now)");
     }
 }
