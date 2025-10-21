49a4b02c41e361ab9f67d67dcbbe07dd210c09db
edwardp20 <zhangxixi201268@outlook.com>
2025-10-21 12:56:45 +0800

    feat: add ArithmeticcoperatorDemo4.java

---
+++ src/main/java/com/myjava/app/ArithmeticcoperatorDemo4.java
@@ -0,0 +1,18 @@
+package com.myjava.app;
+
+
+public class ArithmeticcoperatorDemo4{
+    public static void main(String[] args){
+        char a='a';
+        System.out.println(a+1);
+        
+        
+        
+        
+        
+        
+        
+        
+     
+    }
+}
