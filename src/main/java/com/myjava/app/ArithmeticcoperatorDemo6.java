e6df060e34873c492e4010064e77ef98e002ca7f
edwardp20 <zhangxixi201268@outlook.com>
2025-10-22 19:29:19 +0800

    feat&perf: archive update& add ArithmeticcoperatorDemo6.java

---
+++ src/main/java/com/myjava/app/ArithmeticcoperatorDemo6.java
@@ -0,0 +1,21 @@
+package com.myjava.app;
+
+
+public class ArithmeticcoperatorDemo6{
+    public static void main(String[] args){
+        int x=10;
+        int y=x++;
+        int z=++x;
+        System.out.println(""+x+y+z);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+     
+    }
+}
