4aaaab25d4308a39be8a12249aa9a61c206f95a8
edwardp20 <zhangxixi201268@outlook.com>
2025-10-22 13:16:02 +0800

    feat: ArithmeticcoperatorDemo5.java

---
+++ archive/ArithmeticcoperatorDemoA.java
@@ -0,0 +1,30 @@
+package com.myjava.app;
+
+
+public class ArithmeticcoperatorDemoA{
+    public static void main(String[] args){
+        String a="a";
+        System.out.println(a);
+        System.out.println("a"+12+"sjsj");
+        char b='n';
+        System.out.println(b+12+"sjsj");
+        System.out.println(b+12);
+        System.out.println(b+"gh");
+        System.out.println('v'+12);
+        System.out.println('v'+12+"gggg");
+        //证明成功，双引号类型是string 单引号类型是char
+        int c=18;
+        String k=c+"";
+        System.out.println(k);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+     
+    }
+}
---
+++ src/main/java/com/myjava/app/ArithmeticcoperatorDemo5.java
@@ -0,0 +1,26 @@
+package com.myjava.app;
+
+
+public class ArithmeticcoperatorDemo5{
+    public static void main(String[] args){
+        int c=10;
+        c++;
+        System.out.println(c);//11
+        ++c;
+        System.out.println(c);//12
+        c--;
+        System.out.println(c);//11
+        --c;
+        System.out.println(c);//10
+        
+        
+        
+        
+        
+        
+        
+        
+        
+     
+    }
+}
