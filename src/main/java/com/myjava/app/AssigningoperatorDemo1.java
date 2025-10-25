f6b60b96ce3aa2606c3aa0c0723a6d5dd3f4a40c
edwardp20 <zhangxixi201268@outlook.com>
2025-10-25 14:35:14 +0800

    feat: add AssigningoperatorDemo1.java

---
+++ src/main/java/com/myjava/app/AssigningoperatorDemo1.java
@@ -0,0 +1,23 @@
+package com.myjava.app;
+
+
+public class AssigningoperatorDemo1{
+    public static void main(String[] args){
+        int a=1;
+        System.out.println(a);
+        a += 2;
+        System.out.println(a);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+     
+    }
+}
