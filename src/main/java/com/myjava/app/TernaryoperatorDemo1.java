100e5d2963007961ddb37b87364888151ea98730
edwardp20 <zhangxixi201268@outlook.com>
2025-10-28 22:09:44 +0800

    feat: add TernaryoperatorDemo1.java

---
+++ src/main/java/com/myjava/app/TernaryoperatorDemo1.java
@@ -0,0 +1,12 @@
+package com.myjava.app;
+
+
+public class TernaryoperatorDemo1{
+    public static void main(String[] args){
+        int number1=20;
+        int number2=30;
+        int small=number1<number2 ? number1:number2;
+        System.out.println(small);
+        
+    }
+}
