3b0aead9de6c462cfd160a83f90238b5c66800ea
edwardp20 <zhangxixi201268@outlook.com>
2025-11-02 10:21:36 +0800

    feat: add ForDemo1.java and ForTest1.java

---
+++ src/main/java/com/myjava/tests/ForTest1.java
@@ -0,0 +1,10 @@
+package com.myjava.tests;
+
+
+public class ForTest1{
+    public static void main(String[] args){
+        for(int i=1;i<=5;i++){
+            System.out.println("第"+i+"次执行断线重连业务逻辑");
+        }
+    }
+}
