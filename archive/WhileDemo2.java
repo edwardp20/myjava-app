6cd175697f1708ec73911f857403baf6f0fdbaff
edwardp20 <zhangxixi201268@outlook.com>
2025-11-07 13:04:06 +0800

    feat: add SkipDemo2 and WhileTest

--- archive/WhileDemo2.java
+++ archive/WhileDemo2.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class WhileDemo2{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in); 
-
+        int o="lllk"
         int a=sc.nextInt();
         int tmp=a;
         int count=0;
---
+++ src/main/java/com/myjava/tests/WhileTest.java
@@ -0,0 +1,9 @@
+package com.myjava.tests;
+public class WhileTest{
+    public static void main(String[] args){
+        //这是一个无限循环
+        while(true){
+            System.out.println("成都必吃榜，美味戚雨杨。");
+        }  
+    }  
+} 
