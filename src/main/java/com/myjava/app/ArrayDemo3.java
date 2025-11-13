8293d7bebbdf526af3d2a07efe31d84d1db6d822
edwardp20 <zhangxixi201268@outlook.com>
2025-11-13 15:11:55 +0800

    feat: add ArrayDemo3.java

---
+++ src/main/java/com/myjava/app/ArrayDemo3.java
@@ -0,0 +1,13 @@
+package com.myjava.app;
+/*一个动态列表测试程序*/
+
+
+
+public class ArrayDemo3{
+    public static void main(String[] args){
+        int[] number=new int[10];
+        number[1]=12345;
+        System.out.println(number[0]);
+        System.out.println(number[1]);
+    }
+}
