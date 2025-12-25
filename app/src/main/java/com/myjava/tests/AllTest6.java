96272f663877738d2f484a39adccc0cdc462feb7
edwardp20 <zhangxixi201268@outlook.com>
2025-12-25 21:06:30 +0800

    feat: add AllTest6.java

---
+++ app/src/main/java/com/myjava/tests/AllTest6.java
@@ -0,0 +1,26 @@
+package com.myjava.tests;
+
+
+
+public class AllTest6{
+    public static void main(String[] args){
+        int num=123456;
+        int temp=num;
+        int count=0;
+        while(num != 0){
+            num=num/10;
+            count++;
+        }
+        IO.println(num+" "+count);
+        int[] arr=new int[count];
+        int index=arr.length-1;
+        while(temp!=0){
+            arr[index]=temp%10;
+            temp=temp/10;
+            index--;
+        }
+        for(int i=0;i<arr.length;i++){
+            IO.print(arr[i]+" ");
+        }
+    }
+}
