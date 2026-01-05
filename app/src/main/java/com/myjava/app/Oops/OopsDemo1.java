871a602e253952b45932b8a295e57bbf67979545
edwardp20 <zhangxixi201268@outlook.com>
2026-01-05 20:13:38 +0800

    feat: add OopsDemo1.java and Phone.java

---
+++ app/src/main/java/com/myjava/app/Oops/OopsDemo1.java
@@ -0,0 +1,12 @@
+package com.myjava.app.Oops;
+
+
+public class OopsDemo1{
+    public static void main(String[] args){
+        Phone p=new Phone();
+        p.brand="bbbbb";
+        p.price=114514;
+        IO.println(p.brand+p.price);
+        p.playGame();
+    }
+}
---
+++ app/src/main/java/com/myjava/app/Oops/Phone.java
@@ -0,0 +1,10 @@
+package com.myjava.app.Oops;
+
+
+public class Phone{
+    String brand;
+    int price;
+    public void playGame(){
+        IO.println("bbbb");
+    }
+}
