e90b8adc2142142bf73f580f173ca4bddc3e0bd3
edwardp20 <zhangxixi201268@outlook.com>
2026-01-25 17:56:05 +0800

    fix: now can use test normal

--- app/src/main/java/com/myjava/app/App.java
+++ app/src/main/java/com/myjava/app/App.java
@@ -1,17 +1,14 @@
 package com.myjava.app;
 
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args)
-    {
+/** Hello world! */
+public class App {
+    public static void main(String[] args) {
         System.out.println("helloemacsworld!!!");
-        System.out.println( "你好，欢迎来到本程序，版本请输入java -cp myjava-20251016-SNAPSHOT.jar com.myjava.app.Version" );
+        System.out.println("你好，欢迎来到本程序，版本请输入java -cp myjava-20251016-SNAPSHOT.jar com.myjava.app.Version");
         System.out.println("本程序目前有Copper,Version,ArithmeticcoperatorDemo,App(now),ArithmeticcoperatorDemo2");
-	
+    }
+
+    public static void getGreeting() {
+        IO.println("mmmmmm");
     }
 }
