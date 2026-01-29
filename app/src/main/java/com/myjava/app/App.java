d52658d405e685d7374846751a9480695b9207b2
edwardp20 <zhangxixi201268@outlook.com>
2026-01-29 18:36:28 +0800

    fix: now can display version information normal

--- app/src/main/java/com/myjava/app/App.java
+++ app/src/main/java/com/myjava/app/App.java
@@ -3,9 +3,7 @@ package com.myjava.app;
 /** Hello world! */
 public class App {
     public static void main(String[] args) {
-        System.out.println("helloemacsworld!!!");
-        System.out.println("你好，欢迎来到本程序，版本请输入java -cp myjava-20251016-SNAPSHOT.jar com.myjava.app.Version");
-        System.out.println("本程序目前有Copper,Version,ArithmeticcoperatorDemo,App(now),ArithmeticcoperatorDemo2");
+        System.out.println("你好，欢迎来到本程序，版本请输入java -cp myjava-VERSION.jar com.myjava.app.Version");
     }
 
     public String getGreeting() {
--- app/src/main/java/com/myjava/app/Version.java
+++ app/src/main/java/com/myjava/app/Version.java
@@ -2,7 +2,7 @@ package com.myjava.app;
 
 public class Version{
     void main(String[] args){
-        System.out.println("版本1.14.1");
-        System.out.println("by zhangxixi2012");
+        System.out.println("版本1.15-rc1");
+        System.out.println("by edwardp20");
     }
 }
