64f0da37a98dda82368ef3fe7e024ebabfbc5a9d
edwardp20 <zhangxixi201268@outlook.com>
2026-02-01 14:42:05 +0800

    chore: prepare release 1.15

--- app/src/main/java/com/myjava/app/App.java
+++ app/src/main/java/com/myjava/app/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("你好，欢迎来到本程序，版本请输入java -cp myjava-VERSION.jar com.myjava.app.Version");
     }
-
+    //这个是test
     public String getGreeting() {
         IO.println("mmmmmm");
         return "hello";
