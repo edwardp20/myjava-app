1e55e487129e45bf7bd1bfe2ce754ce94755a175
edwardp20 <zhangxixi201268@outlook.com>
2026-05-05 18:03:44 +0800

    feat: add ArrayListDemo2.java

---
+++ app/src/main/java/com/myjava/app/arraylistdemo/ArrayListDemo2.java
@@ -0,0 +1,34 @@
+package com.myjava.app.arraylistdemo;
+
+import java.util.ArrayList;
+/**
+* ArrayListDemo2 - 
+* 
+* Copyright (C) 2026 edwardp20 <zhangxixi201268@outlook.com>
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+public class ArrayListDemo2 {
+    public static void main(String[] args) {
+        //首先创建一个数组
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        //添加元素
+        arr.add(6446);
+        arr.add(646);
+        arr.add(6366);
+        arr.add(369);
+        //打印数组
+        System.out.println(arr);
+    }
+}
